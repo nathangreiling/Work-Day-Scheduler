4453d5680781253d047cfef1d52a4ed082f443bc
nathangreiling <ngreiling20@gmail.com>
2022-03-13 19:25:38 -0500

    color codes time blocks according to current time
    New function in JS compares the current time to the integer value of the time in the individual time blocks and displays them accordingly in relation to the current time.
    Minor changes had to be made to the html to get this function to work properly. Changed integer value of the times in the html to work with moment.js times.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
-
+// grabs current date and generates it in the html
 var displayCurrentTime = document.getElementById("currentDay");
 var currentTime = moment().format("MMM Do YY");
 displayCurrentTime.textContent = currentTime;
+
+// displays the tense of the time blocks according to the current time 
+function timeSlots () {
+    $(".form-control").each(function () {
+        var blockTime = parseInt($(this).attr("id"));
+        var currentHour = moment().hours();
+        currentHour = parseInt(currentHour);
+
+        if (currentHour > blockTime) {
+            $(this).addClass("past");
+        } else if (currentHour === blockTime){
+            $(this).addClass("present");
+        } else if (currentHour < blockTime) {
+            $(this).addClass("future");
+        }
+    });
+  }
+timeSlots();
+
