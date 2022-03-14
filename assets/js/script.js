bef72c054bbf14825f85743be4d56911c4f9ecc5
nathangreiling <ngreiling20@gmail.com>
2022-03-13 22:59:06 -0500

    added function to JS to return local storage
    Created function "loadTasks" which contains variables that parse the data already stored in the local storage. These variables containing the parsed data are then used to generate the text content on the page by selecting the element by ID and setting the text value equal to their value.

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,19 +26,34 @@ timeSlots();
 var saveTasks = function(){
     localStorage.setItem(taskTime, JSON.stringify(userInput));
 }
-
-var saveBtn = document.querySelector('.saveBtn');
-
+var saveBtn = document.querySelector(".saveBtn");
 $(".saveBtn").on("click", function(){
     taskTime = $(this).siblings(".input-group-prepend").text().trim();
     userInput = $(this).siblings(".form-control").val().trim();
     saveTasks();
 });
 
-// ----
-var loadTasks = function(){}
+// takes the data in local storage, parses it, and returns it to the corresponding row 
+var loadTasks = function(){
+    var saved9 = JSON.parse(localStorage.getItem("09:00"));
+    var saved10 = JSON.parse(localStorage.getItem("10:00"));
+    var saved11 = JSON.parse(localStorage.getItem("11:00"));
+    var saved12 = JSON.parse(localStorage.getItem("12:00"));
+    var saved13 = JSON.parse(localStorage.getItem("13:00"));
+    var saved14 = JSON.parse(localStorage.getItem("14:00"));
+    var saved15 = JSON.parse(localStorage.getItem("15:00"));
+    var saved16 = JSON.parse(localStorage.getItem("16:00"));
+    var saved17 = JSON.parse(localStorage.getItem("17:00"));
+    document.getElementById("09:00").textContent = saved9;
+    document.getElementById("10:00").textContent = saved10;
+    document.getElementById("11:00").textContent = saved11;
+    document.getElementById("12:00").textContent = saved12;
+    document.getElementById("13:00").textContent = saved13;
+    document.getElementById("14:00").textContent = saved14;
+    document.getElementById("15:00").textContent = saved15;
+    document.getElementById("16:00").textContent = saved16;
+    document.getElementById("17:00").textContent = saved17;
+}
 
-
-
-
+loadTasks();
 
